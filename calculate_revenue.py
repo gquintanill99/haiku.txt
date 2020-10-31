4d49e6c161ebc7004521bf9c622edd861d94400d
Guillermo Quintanilla <73296235+gquintanill99@users.noreply.github.com>
2020-10-31 11:55:37 -0600

    py

---
+++ calculate_revenue.py
@@ -0,0 +1,11 @@
+sales = 150
+average_price=2100
+cogs_percentage=0.59
+
+annual_gross_revenue = (sales*average_price)
+monthly_gross_revenue= annual_gross_revenue/12
+quarterly_gross_revenue=annual_gross_revenue/4
+
+print(annual_gross_revenue)
+print(monthly_gross_revenue)
+print(quarterly_gross_revenue)
